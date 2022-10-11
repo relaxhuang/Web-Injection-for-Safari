494915564bf2c17e50dda0553292a2dc669467a4
relax0914 <relax0914@gmail.com>
2022-10-11 13:27:47 +0800

    Initial Commit

---
+++ Web Injection for Safari/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Web Injection for Safari
+//
+//  Created by James Huang on 2022/10/11.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
